d314f8ddf19ebc6e0322451e3e0a82f21d7b0941
emwu1024 <emwu1024@gmail.com>
2023-06-08 15:04:57 +0100

    fix navbar (partial fox)

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,47 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive((prevIsActive) => !prevIsActive);
+  };
+
   return (
-    <>
-      {/*  Navbar  */}
-      <nav className="navbar">
-        <div className="navbar-container">
-          <a href="/" id="navbar-logo">
-            <img src="logo-transparent-beige4.png" alt="Ukulele Repo Logo" />
-            {/* <i className="fa-solid fa-guitar"></i>Ukulele */}
-          </a>
-          <div className="navbar-toggle" id="mobile-menu">
-            <span className="bar"></span>
-            <span className="bar"></span>
-            <span className="bar"></span>
-          </div>
-          <ul className="navbar-menu">
-            <li className="navbar-item">
-              <NavLink to="/" className="navbar-links">
-                Home
-              </NavLink>
-            </li>
-            <li className="navbar-item">
-              <NavLink to="/songs" className="navbar-links">
-                Songs
-              </NavLink>
-            </li>
-            <li className="navbar-item">
-              <NavLink to="/faq" className="navbar-links">
-                FAQ
-              </NavLink>
-            </li>
-            <li className="navbar-btn">
-              <NavLink className="signup-btn" to="/signup">
-                Sign Up
-              </NavLink>
-            </li>
-          </ul>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <NavLink to="/" id="navbar-logo">
+          <img
+            src="logo-transparent-beige4.png"
+            alt="Ukulele Repo Logo"
+            height="80px"
+          />
+        </NavLink>
+        <div
+          className={`navbar-toggle ${isActive ? "is-active" : ""}`}
+          id="mobile-menu"
+          onClick={toggleMenu}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
         </div>
-      </nav>
-    </>
+        <ul className={`navbar-menu ${isActive ? "active" : ""}`}>
+          <li className="navbar-item">
+            <NavLink to="/" className="navbar-links">
+              Home
+            </NavLink>
+          </li>
+          <li className="navbar-item">
+            <NavLink to="/songs" className="navbar-links">
+              Songs
+            </NavLink>
+          </li>
+          <li className="navbar-item">
+            <NavLink to="/faq" className="navbar-links">
+              FAQ
+            </NavLink>
+          </li>
+          <li className="navbar-btn">
+            <NavLink className="signup-btn" to="/signup">
+              Sign Up
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
